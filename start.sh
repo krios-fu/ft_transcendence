e0f2eeed730c76512af6e7d84901ff9ccb4187f2
Daniel Rodriguez pueyo <danrodri@c3r6s2.42madrid.com>
2022-05-11 18:56:18 +0200

    ...

--- start.sh
+++ start.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+if [ "$1" = "purge" ]; then
+    docker rm $(docker ps -a -q)
+    docker rmi --force $(docker images -q)
+    docker volume rm $(docker volume ls -q)
+
+    exit 0;
+fi
+
 docker-compose up --build
