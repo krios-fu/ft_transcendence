5a35c5aab4a2c7fa97616169107b151a2c4ca88e
Kevin Rios fuentes <krios-fu@c3r10s3.42madrid.com>
2022-05-13 17:56:54 +0200

    install typeORM Y driver pg

--- backend/app/api/src/app.module.ts
+++ backend/app/api/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
