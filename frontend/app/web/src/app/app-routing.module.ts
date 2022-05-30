c90d9eef1394b9681a2c72091d0d7d3b46e32436
Kevin Rios fuentes <krios-fu@c2r1s4.42madrid.com>
2022-05-30 03:12:03 +0200

    mod /login create route /

--- frontend/app/web/src/app/app-routing.module.ts
+++ frontend/app/web/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from "./login/login.component";
+import {LoginModule} from "./login/login.module";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component : LoginComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule, LoginModule]
 })
 export class AppRoutingModule { }
