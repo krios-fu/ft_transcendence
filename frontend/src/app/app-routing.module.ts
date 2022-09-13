48ce116655c4e51acc82086fe03c93b3adc5c74f
onapoli <onapoli_dev@protonmail.com>
2022-09-13 14:32:27 +0200

    Add game component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GameComponent } from './game/game.component';
 
-const routes: Routes = [];
+/*
+**  An empty path corresponds to / route.
+*/
+const routes: Routes = [
+  { path: '', title: 'Transcendental PONG', component: GameComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
