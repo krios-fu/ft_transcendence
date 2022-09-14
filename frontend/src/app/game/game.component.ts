f33e86874304c1f0fe371d757bc0c3d269c041c9
onapoli <onapoli_dev@protonmail.com>
2022-09-14 20:13:13 +0200

    Add game initialization logic to GameComponent

--- frontend/src/app/game/game.component.ts
+++ frontend/src/app/game/game.component.ts
@@ -1,10 +1,56 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core"
+import * as Phaser from 'phaser'
+import * as SockIO from 'socket.io-client'
+import { PlayerAScene } from "./scenes/PlayerAScene";
+import { PlayerBScene } from "./scenes/PlayerBScene";
+import { SpectatorScene } from "./scenes/SpectatorScene";
 
 @Component({
     selector: 'app-game',
     templateUrl: './game.component.html',
     styleUrls: ['./game.component.css']
 })
-export class GameComponent {
-    
+export class    GameComponent implements OnInit {
+
+    private config: Phaser.Types.Core.GameConfig;
+    private socket: SockIO.Socket;
+    private game?: Phaser.Game;
+
+    constructor () {
+        this.config = {
+            type: Phaser.AUTO, //WEBGL if available. Canvas otherwise.
+            parent: 'game_zone',
+            width: 800,
+            height: 600,
+            /*scale: {
+                mode: Phaser.Scale.WIDTH_CONTROLS_HEIGHT ,
+                autoCenter: Phaser.Scale.CENTER_BOTH
+            },*/
+            scene: undefined // Will be assigned afterwards
+        };
+        this.socket = SockIO.io("ws://localhost:3001");
+    }
+
+    ngOnInit(): void {
+        //Create dto for data
+        this.socket.once("role", (data: any) => { //PlayerA, PlayerB, Spectator
+            if (data.role === "PlayerA")
+            {
+                this.config.scene = new PlayerAScene(
+                                        this.socket, data.room, data.initData);
+            }
+            else if (data.role === "PlayerB")
+            {
+                this.config.scene = new PlayerBScene(
+                                        this.socket, data.room, data.initData);
+            }
+            else if (data.role === "Spectator")
+            {
+                this.config.scene = new SpectatorScene(
+                                        this.socket, data.room, data.initData);
+            }
+            // Add else to handle invalid data.role ?
+            this.game = new Phaser.Game(this.config);
+        })
+    }
 }
