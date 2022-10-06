41ffdd42c3c463154091a49615a52b58ceb9af9c
onapoli <onapoli_dev@protonmail.com>
2022-10-06 13:47:08 +0200

    Import match and user modules to game module

--- backend/app/api/src/game/game.module.ts
+++ backend/app/api/src/game/game.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MatchModule } from 'src/match/match.module';
+import { UserModule } from 'src/user/user.module';
 import { GameGateway } from './game.gateway';
+import { GameService } from './game.service';
 
 @Module({
-  providers: [GameGateway],
+  imports: [
+    MatchModule,
+    UserModule
+  ],
+  providers: [
+    GameGateway,
+    GameService
+  ],
 })
 export class GameModule {}
