a7b547df18c108be379dcda33d2b1afbc24fc672
onapoli <onapoli_dev@protonmail.com>
2022-10-12 19:54:51 +0200

    Import User, Winner & Loser modules in game module

--- backend/app/api/src/game/game.module.ts
+++ backend/app/api/src/game/game.module.ts
@@ -1,13 +1,17 @@
 import { Module } from '@nestjs/common';
+import { LoserModule } from 'src/match/loser/loser.module';
 import { MatchModule } from 'src/match/match.module';
+import { WinnerModule } from 'src/match/winner/winner.module';
 import { UserModule } from 'src/user/user.module';
 import { GameGateway } from './game.gateway';
 import { GameService } from './game.service';
 
 @Module({
   imports: [
+    UserModule,
     MatchModule,
-    UserModule
+    WinnerModule,
+    LoserModule
   ],
   providers: [
     GameGateway,
