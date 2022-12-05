b54045a6f3c892d79248d15ade5e9ec67e1b5df0
onapoli <onapoli_dev@protonmail.com>
2022-12-05 12:54:46 +0100

    Add game start titles

--- frontend/src/app/game/scenes/StartScene.ts
+++ frontend/src/app/game/scenes/StartScene.ts
@@ -1,10 +1,13 @@
 import * as SocketIO from 'socket.io-client'
 import { IMatchInitData } from '../elements/Match';
+import { StartTitles } from '../elements/StartTitles';
 import { Txt } from '../elements/Txt';
 import { BaseScene } from './BaseScene'
 import { IMenuInit } from './MenuScene';
 
 export class    StartScene extends BaseScene {
+
+    startTitles?: StartTitles;
 
     constructor(
         sock: SocketIO.Socket, room: string
@@ -14,7 +17,7 @@ export class    StartScene extends BaseScene {
 
     init() {
         this.socket.once("newGame", (data: IMenuInit) => {
-            this.initTxt?.destroy();
+            this.startTitles?.destroy();
             this.removeAllSocketListeners();
             if (data.hero)
                 this.scene.start("MenuHero", data);
@@ -22,31 +25,18 @@ export class    StartScene extends BaseScene {
                 this.scene.start("Menu", data);
         });
         this.socket.once("startMatch", (gameData: IMatchInitData) => {
-            this.initTxt?.destroy();
+            this.startTitles?.destroy();
             this.removeAllSocketListeners();
             this.scene.start("Spectator", gameData);
         });
         this.socket.once("end", (data: any) => {
-            this.initTxt?.destroy();
+            this.startTitles?.destroy();
             this.removeAllSocketListeners();
             this.scene.start("End", data);
         });
     }
 
-    createInitText() {
-        //Init screen setup
-        this.initTxt = new Txt(this, {
-            xPos: 400,
-            yPos: 250,
-            content: `Waiting for players ...`,
-            style: { fontSize: '20px', color: '#fff' },
-            xOrigin: 0.5,
-            yOrigin: 0.5,
-            depth: 1
-        });
-    }
-
     create() {
-        this.createInitText();
+        this.startTitles = new StartTitles(this);
     }
 }
