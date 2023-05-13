d17fddf42a23aa668205eb204ef9a2760773eb79
juzzrespecter <danisarp@gmail.com>
2023-05-13 21:06:25 +0200

    problemas con peticiones desde front a back

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -10,7 +10,7 @@ import { AuthService } from '../services/auth.service';
 export class LoginComponent implements OnInit {
 
   constructor( private http : HttpClient, private authService: AuthService) { }
-  loginUrl = 'https://api.intra.42.fr/oauth/authorize?client_id=69aeb66a278743631dbafcd44c86243a16b425b19a096d176dc681ae7fadc3dd&redirect_uri=http%3A%2F%2Flocalhost%3A4200%2Fhome&response_type=code';
+  loginUrl = 'https://api.intra.42.fr/oauth/authorize?client_id=69aeb66a278743631dbafcd44c86243a16b425b19a096d176dc681ae7fadc3dd&redirect_uri=https%3A%2F%2Fc1r15s3%2Fhome%2F&response_type=code';
   ngOnInit(): void {
     if (this.authService.isAuthenticated())
       this.authService.redirectHome();
