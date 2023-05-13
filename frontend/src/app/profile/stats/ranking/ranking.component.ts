3302ebf28fa0affade6b01fe2c8a6b1feff61d57
onapoli <onapoli_dev@protonmail.com>
2023-05-13 16:38:09 +0200

    Show profile user ranking list page initially

--- frontend/src/app/profile/stats/ranking/ranking.component.ts
+++ frontend/src/app/profile/stats/ranking/ranking.component.ts
@@ -1,30 +1,40 @@
 import {
-    AfterViewInit,
     Component,
+    OnDestroy,
     OnInit
 } from '@angular/core';
 import {
     RankingData,
     RankingService,
-    UserRanking
+    UserCountData
 } from './ranking.service';
 import { PageEvent } from '@angular/material/paginator';
-import { Observable } from 'rxjs';
+import {
+    Observable,
+    Subscription
+} from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
     selector: 'app-ranking',
     templateUrl: './ranking.component.html',
     styleUrls: ['./ranking.component.scss']
 })
-export class RankingComponent implements OnInit, AfterViewInit {
+export class RankingComponent implements OnInit, OnDestroy {
 
     ranking: RankingData[];
     displayedColumns: string[];
     totalUsers: number;
     pageSize: number;
     pageIndex: number;
+    username: string;
+    
+    private _routeParams?: Subscription;
 
-    constructor(private readonly rankingService: RankingService) {
+    constructor(
+        private readonly rankingService: RankingService,
+        private route: ActivatedRoute
+    ) {
         this.ranking = [];
         this.displayedColumns = [
             "rank",
@@ -35,22 +45,42 @@ export class RankingComponent implements OnInit, AfterViewInit {
         this.totalUsers = 0;
         this.pageSize = 10;
         this.pageIndex = 0;
+        this.username = "";
     }
 
     ngOnInit(): void {
-        console.log("Initiated ranking component");
+        this._routeParams = this.route.params.subscribe(({ id }) => {
+            this.username = id;
+            this.updateRankings(this.username);
+        });
     }
 
-    ngAfterViewInit(): void {
-        this.updateRankings();
+    private _requestRankingData(targetUser?: string)
+                                    : Observable<UserCountData> {    
+        if (targetUser)
+        {
+            return (
+                this.rankingService.getInitialRankings(
+                    this.pageSize,
+                    targetUser
+                )
+            );
+        }
+        return (
+            this.rankingService.getRankings(
+                this.pageSize,
+                this.pageSize * this.pageIndex
+            )
+        );
     }
 
-    updateRankings(): void {
-        this.rankingService.getRankings(
-            this.pageSize,
-            this.pageSize * this.pageIndex
-        ).subscribe({ // No need to unsubscribe. Only one next value
-            next: (data: [UserRanking[], number]) => {
+    updateRankings(targetUser?: string): void {
+        this._requestRankingData(targetUser)
+        .subscribe({ // No need to unsubscribe. Only one next value
+            next: (data: UserCountData) => {
+                if (data[2] != undefined
+                        && data[2] != 0)
+                    this.pageIndex = Math.floor(data[2] / this.pageSize);
                 this.ranking = this.rankingService.userToRanking(
                     data[0],
                     this.pageIndex * this.pageSize + 1
@@ -66,6 +96,10 @@ export class RankingComponent implements OnInit, AfterViewInit {
     pageEventHandler(pageEvent: PageEvent): void {
         this.pageIndex = pageEvent.pageIndex;
         this.updateRankings();
+    }
+
+    ngOnDestroy(): void {
+        this._routeParams?.unsubscribe();
     }
 
 }
