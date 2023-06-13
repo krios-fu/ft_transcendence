1f43ef41ff6c48494b6d84bbfe3efb7957d1da9f
onapoli <onapoli_dev@protonmail.com>
2023-06-13 21:02:08 +0200

    seed db with multiple admins instead of single

--- backend/migrations/1686417455407-init_rooms.ts
+++ backend/migrations/1686417455407-init_rooms.ts
@@ -1,36 +1,54 @@
 import { MigrationInterface, QueryRunner } from "typeorm"
 import { RoomEntity } from "../src/room/entity/room.entity"
 import { UserEntity } from "../src/user/entities/user.entity";
+import { CreatorUsername } from "./1686412674045-init_users";
 
 export type    RoomName = "atlantis" |
                             "metropolis" |
                             "wakanda";
 
+function    getCreator(creators: UserEntity[],
+                        targetUsername: CreatorUsername): UserEntity {
+    return (
+        creators.find(creator => creator.username === targetUsername)
+    );
+}
+
 export class initRooms1686417455407 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        const   adminEntity: UserEntity = await queryRunner.manager
-                                                    .getRepository(UserEntity)
-                                                    .findOneBy({id: 1});
+        const   creatorEntities: UserEntity[] = await queryRunner.manager
+                    .getRepository(UserEntity)
+                    .findBy([
+                        {
+                            username: "danrodri" as CreatorUsername
+                        },
+                        {
+                            username: "krios-fu" as CreatorUsername
+                        },
+                        {
+                            username: "onapoli-" as CreatorUsername
+                        }
+                    ]);
     
         await queryRunner.manager.getRepository(RoomEntity).insert([
             {
                 roomName: "atlantis" as RoomName,
-                ownerId: adminEntity.id,
-                photoUrl: "a",
-                owner: adminEntity,
+                ownerId: getCreator(creatorEntities, "onapoli-").id,
+                photoUrl: "http://localhost:3000/static/rooms/atlantis.png",
+                owner: getCreator(creatorEntities, "onapoli-"),
             },
             {
                 roomName: "metropolis" as RoomName,
-                ownerId: adminEntity.id,
-                photoUrl: "b",
-                owner: adminEntity,
+                ownerId: getCreator(creatorEntities, "krios-fu").id,
+                photoUrl: "http://localhost:3000/static/rooms/metropolis.png",
+                owner: getCreator(creatorEntities, "krios-fu"),
             },
             {
                 roomName: "wakanda" as RoomName,
-                ownerId: adminEntity.id,
-                photoUrl: "c",
-                owner: adminEntity,
+                ownerId: getCreator(creatorEntities, "danrodri").id,
+                photoUrl: "http://localhost:3000/static/rooms/wakanda.png",
+                owner: getCreator(creatorEntities, "danrodri"),
             }
         ]);
     }
--- backend/migrations/1686496939782-init_user_roles.ts
+++ backend/migrations/1686496939782-init_user_roles.ts
@@ -3,36 +3,75 @@ import { UserRolesEntity } from "../src/user_roles/entity/user_roles.entity"
 import { UserEntity } from "../src/user/entities/user.entity";
 import { RolesEntity } from "../src/roles/entity/roles.entity";
 import { RoleName } from "./1686493785757-init_roles";
+import { CreatorUsername } from "./1686412674045-init_users";
 
 export class initUserRoles1686496939782 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        const   adminEntity: UserEntity = await queryRunner.manager
-                                                    .getRepository(UserEntity)
-                                                    .findOneBy({
-                                                        username: "admin"
-                                                    });
+        const   creatorEntities: UserEntity[] = await queryRunner.manager
+                    .getRepository(UserEntity)
+                    .findBy([
+                        {
+                            username: "danrodri" as CreatorUsername
+                        },
+                        {
+                            username: "krios-fu" as CreatorUsername
+                        },
+                        {
+                            username: "onapoli-" as CreatorUsername
+                        }
+                    ]);
         const   adminRoleEntity: RolesEntity = await queryRunner.manager
-                                                    .getRepository(RolesEntity)
-                                                    .findOneBy({
-                                                        role: "admin" as RoleName
-                                                    });
+                    .getRepository(RolesEntity)
+                    .findOneBy({
+                        role: "admin" as RoleName
+                    });
         
-        await queryRunner.manager.getRepository(UserRolesEntity).insert({
-            userId: adminEntity.id,
-            user: adminEntity,
-            roleId: adminRoleEntity.id,
-            role: adminRoleEntity,
-        });
+        await queryRunner.manager.getRepository(UserRolesEntity).insert(
+            creatorEntities.map(creator => {
+                return ({
+                    userId: creator.id,
+                    user: creator,
+                    roleId: adminRoleEntity.id,
+                    role: adminRoleEntity,
+                })
+            })
+        );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.manager.getRepository(UserRolesEntity).delete({
-            userId: (await queryRunner.manager.getRepository(UserEntity)
-                                                .findOneBy({
-                                                    username: "admin"
-                                                })).id
-        });
+        const   creatorIds: number[] = (await queryRunner.manager
+                    .getRepository(UserEntity)
+                    .find({
+                        where: [
+                            {
+                                username: "krios-fu" as CreatorUsername
+                            },
+                            {
+                                username: "danrodri" as CreatorUsername
+                            },
+                            {
+                                username: "onapoli-" as CreatorUsername
+                            }
+                        ]
+                    })).map((user: UserEntity) => {
+                        return (user.id);
+                    });
+        const   userRoleIds: number[] = (await queryRunner.manager
+                    .getRepository(UserRolesEntity)
+                    .find({
+                        where: creatorIds.map(id => {
+                            return ({
+                                id: id
+                            });
+                        })
+                    })).map(userRoleEntity => {
+                        return (userRoleEntity.id);            
+                    });
+        
+        await queryRunner.manager.getRepository(UserRolesEntity).delete(
+            userRoleIds
+        );
     }
 
 }
