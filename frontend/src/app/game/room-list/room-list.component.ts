07c4b99bd2203150a9c4d15e01b7b32264a1b003
onapoli <onapoli_dev@protonmail.com>
2023-06-17 18:37:18 +0200

    Missing statement at room usercount assignment

--- frontend/src/app/game/room-list/room-list.component.ts
+++ frontend/src/app/game/room-list/room-list.component.ts
@@ -91,6 +91,7 @@ export class    RoomListComponent implements OnInit {
                 }
                 for (const [id, room] of roomUserCount.entries())
                 {
+                    this.rooms[id].userCount = room.userCount;
                 }
             },
             error: (err: any) => {
