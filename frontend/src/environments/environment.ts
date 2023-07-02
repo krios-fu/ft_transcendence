150a86f46d48e418c74abed3f2931f501a1c6d2a
Macondo <macondo@macondo.local>
2023-07-02 16:18:21 +0200

    mod wsUrl

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:3000/',
-  wsUrl: "ws://localhost:3000/",
+  wsUrl: "ws://localhost:3001/",
   redirectUri: 'https://api.intra.42.fr/oauth/authorize?client_id=4fa51aeb8eafcafd00c2b72a70720daf534190b81adf41fb9874c83bb9563042&redirect_uri=http%3A%2F%2Flocalhost%2Flogin&response_type=code'
 };
 
