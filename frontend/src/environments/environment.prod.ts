88e55566e2085a9e1680f01884155fefd517e4a4
juzzrespecter <danisarp@gmail.com>
2023-07-09 16:53:57 +0200

    nada

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   apiUrl: '/api/',
   wsUrl: '/',
   staticUrl: "/static/",
-  redirectUri: 'https://api.intra.42.fr/oauth/authorize?client_id=4fa51aeb8eafcafd00c2b72a70720daf534190b81adf41fb9874c83bb9563042&redirect_uri=http%3A%2F%2Flocalhost%2Flogin&response_type=code'
+  redirectUri: 'https://api.intra.42.fr/oauth/authorize?client_id=69aeb66a278743631dbafcd44c86243a16b425b19a096d176dc681ae7fadc3dd&redirect_uri=https%3A%2F%2Fc1r12s5%2Flogin&response_type=code'
 };
